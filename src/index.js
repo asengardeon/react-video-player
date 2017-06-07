3deb07ffe4b2acb12ed91a0ad7db134752d5280b
Leandro Vilson Battisti <asengardeons@hotmail.com>
2017-06-06 23:11:39 -0300

    first steps in video player with react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//Youtube API Key AIzaSyDMm5O3WEGQ1MSZv5qwiivCOw0xe8JREO8
+const API_KEY = 'AIzaSyDMm5O3WEGQ1MSZv5qwiivCOw0xe8JREO8';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+// Create a nre component and this component should produces HTML
+ class App extends Component{
+     constructor(props) {
+         super(props);
+         this.state = { videos: []};
+
+         YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+                     this.setState({videos})
+                     //this is equal to this.setState({videos: videos})
+                  });
+
+    }
+
+
+     render() {
+        return(<div>
+                  <SearchBar />
+                  <VideoDetail video={this.state.videos[0]}/>
+                  <VideoList videos={this.state.videos}/>
+               </div>
+              )}
+ }
+
+
+ReactDom.render(<App/>, document.querySelector('.container'));
+
+
+//take this component's generated html  amd put it on the page(DOM Tree)
